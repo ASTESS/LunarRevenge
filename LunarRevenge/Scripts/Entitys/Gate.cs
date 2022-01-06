7a00ee554da94de9543ec818eb4c42c77ada6551
matthiasvandesande <55184257+matthiasvandesande@users.noreply.github.com>
2022-01-06 15:56:15 +0100

    Gate Update

--- LunarRevenge/Scripts/Entitys/Gate.cs
+++ LunarRevenge/Scripts/Entitys/Gate.cs
@@ -4,18 +4,21 @@ using LunarRevenge.Scripts.World.Textures;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace LunarRevenge.Scripts.Entitys
 {
+    enum GateState { Open, Closed };
+
     internal class Gate : Entity
     {
+
+        public GateState CurrentGateState = GateState.Closed;
         private Vector2 postition = new Vector2(0, 0);
         TextureManager textureManager;
         private bool canUpdate = true;
         private bool doorOpen = true;
         string key = "animated_smallgate_1";
+
         public Gate(Texture2D texture, Vector2 pos, Collision collision, string name, TextureManager textureManager) : base(texture, collision, name)
         {
             this.health = 50;
@@ -28,13 +31,13 @@ namespace LunarRevenge.Scripts.Entitys
         {
             pos = new Vector2(postition.X + Player.offset.X, postition.Y + Player.offset.Y);
 
-            if (Distance(LevelScreen.entitys["player"]) > 100)
+            if (Distance(LevelScreen.entitys["player"]) > 75)
             {
-
+                closeGate();
             }
             else
             {
-
+                openGate();
             }
 
             updateTimer(gameTime);
@@ -52,7 +55,38 @@ namespace LunarRevenge.Scripts.Entitys
             }
         }
 
-        public override void Animation(GameTime gameTime)
+        public void openGate()
+        {
+            if(CurrentGateState.Equals(GateState.Closed))
+            {
+                CurrentGateState = GateState.Open;
+                Console.WriteLine("open");
+
+                // Animation for opening the gate
+
+                // remove temporary collision
+
+            }
+            
+        }
+
+        public void closeGate()
+        {
+            if (CurrentGateState.Equals(GateState.Open))
+            {
+                CurrentGateState = GateState.Closed;
+                Console.WriteLine("closed");
+
+                // Animation for closing the gate
+
+                // Add temporary colission
+
+            }
+        }
+
+
+
+        public void _Animation(GameTime gameTime)
         {
             startingY = 672;
             width = 32;
@@ -89,6 +123,8 @@ namespace LunarRevenge.Scripts.Entitys
                     newItem += split[i];
                     newItem += '_';
                 }
+
+                Console.WriteLine(newItem);
 
                 string test = newItem + number.ToString();
                 if (textureManager.worldTextures.ContainsKey(test))
